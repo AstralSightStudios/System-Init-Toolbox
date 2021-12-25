b6bce5821d803f98cdddad2447818b82eac2c1ee
Searchstars <2039210460@qq.com>
2021-12-25 11:43:43 +0800

    System.IO错误 待修复

--- net_framework.xaml.cs
+++ net_framework.xaml.cs
@@ -11,10 +11,19 @@ namespace System_Init_Toolbox
     /// </summary>
     public partial class net_framework
     {
-        private static Stream wb;
+        private bool downloading = false;
+        private Stream wb;
         public net_framework()
         {
             InitializeComponent();
+        }
+        public void _download_window_closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (downloading)
+            {
+                //若窗口关闭即停止下载
+                wb.Close();
+            }
         }
         private static string BytesToString(decimal Bytes)
         {
@@ -38,7 +47,7 @@ namespace System_Init_Toolbox
                 return string.Format("{0:0} KB", Kb);
             }
         }
-        public static bool DownloadFile(string URL, FileStream fs, System.Windows.Controls.ProgressBar progressBar1, System.Windows.Controls.Label label1, driver_download_window driver_download_window)
+        public static bool DownloadFile(string URL, FileStream fs, System.Windows.Controls.ProgressBar progressBar1, System.Windows.Controls.Label label1, net_framework net_framework)
         {
             try
             {
@@ -75,7 +84,28 @@ namespace System_Init_Toolbox
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            down_and_install_button.IsEnabled = false;
+            progressBar1.IsIndeterminate = true;
+            status_label.Content = "正在安装.NET Framework 3.5";
+            Process bat35_Process = new Process();
+            bat35_Process.StartInfo.FileName = "./BATFiles/dotnet-framework-3.5-on.bat";
+            bat35_Process.StartInfo.CreateNoWindow = true;
+            bat35_Process.Start();
+            progressBar1.IsIndeterminate=false;
+            FileStream down_452_fs = new FileStream("netframework452.exe",FileMode.OpenOrCreate);
+            status_label.Content = "正在下载.NET Framework 4.5.2";
+            downloading = true;
+            DownloadFile("https://gitee.com/search__stars/uris_-system_-init_-toolbox/attach_files/924577/download/netframework452.exe", down_452_fs, progressBar1, label1, this);
+            wb.Close();
+            downloading = false;
+            status_label.Content = "正在安装.NET Framework 4.5.2";
+            progressBar1.IsIndeterminate = true;
+            label1.Content = " ";
+            Process dotnet452_Process = new Process();
+            ProcessStartInfo startInfo_452 = new ProcessStartInfo("./netframework452.exe", "/q /norestart /ChainingPackage FullX64Bootstrapper");
+            dotnet452_Process.StartInfo = startInfo_452;
+            dotnet452_Process.StartInfo.CreateNoWindow = true;
+            dotnet452_Process.Start();
         }
     }
 }
